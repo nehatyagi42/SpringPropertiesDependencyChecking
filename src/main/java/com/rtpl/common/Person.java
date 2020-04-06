0dbe7744f50d39b25623c0f976ecb6b46b9de317
neha <nehatyagi2516@gmail.com>
2020-04-06 18:18:56 +0530

    Successful Committed

---
+++ src/main/java/com/rtpl/common/Person.java
@@ -0,0 +1,31 @@
+package com.rtpl.common;
+
+public class Person{
+	
+	private String name;
+	private String Address;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", Address=" + Address + ", age=" + age + "]";
+	}
+	
+}
